bbf92890952bfeef9231bcd29303b98e90c693e2
Zulfikar Muhamad <zul.m@icloud.com>
2025-06-20 11:22:41 +0800

    Used local provider and added app service plan

--- main.tf
+++ main.tf
@@ -4,10 +4,40 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.31.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.3"
+    }
   }
 }
 
+locals {
+  app_service_name        = "${var.resource_group_name}-as"
+  app_service_plan_name   = "${var.resource_group_name}-asp"
+  app_service_plan_os     = var.app_service_plan_os
+  app_service_plan_sku    = var.app_service_plan_sku
+  resource_group_location = var.resource_group_location
+  resource_group_name     = var.resource_group_name
+}
+
 resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+  name     = local.resource_group_name
+  location = local.resource_group_location
+}
+
+resource "azurerm_service_plan" "app_service_plan" {
+  name                = local.app_service_plan_name
+  resource_group_name = local.resource_group_name
+  location            = local.resource_group_location
+  sku_name            = local.app_service_plan_sku
+  os_type             = local.app_service_plan_os
+}
+
+resource "azurerm_windows_web_app" "app_sevice" {
+  name                = local.app_service_name
+  resource_group_name = local.resource_group_name
+  location            = local.resource_group_location
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+
+  site_config {}
 }
--- providers.tf
+++ providers.tf
@@ -3,3 +3,6 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
+
+provider "local" {
+}
